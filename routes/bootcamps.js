8f7117031212a2972422288927bc3ca617ad3e13
muhammadammar42000 <muhammadammar42000@gmail.com>
2023-04-26 18:05:31 +0500

    auth protected middleware added ... like only admin/publisher able to CRUD bootcamp and courses

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -16,19 +16,23 @@ const courseRouter = require("./courses");
 
 const router = express.Router();
 
+const { protect, authorize } = require("../middleware/auth");
+
 //Re-route into other resource router
 router.use("/:bootcampId/courses", courseRouter);
 
-router.route("/:id/photo").put(bootcampPhotoUpload);
+router
+  .route("/:id/photo")
+  .put(protect, authorize("publisher", "admin"), bootcampPhotoUpload);
 
 router
   .route("/")
   .get(advancedResults(Bootcamp, "courses"), getBootcamps)
-  .post(createBootcamp);
+  .post(protect, authorize("publisher", "admin"), createBootcamp);
 router
   .route("/:id")
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, authorize("publisher", "admin"), updateBootcamp)
+  .delete(protect, authorize("publisher", "admin"), deleteBootcamp);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const errorHandler = require("./middleware/error");
 const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -17,11 +18,15 @@ connectDB();
 //Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
 //Body Parser
 app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
 
 //middleware
 // app.use(logger);
@@ -38,6 +43,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
